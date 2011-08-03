b7605d8ad93cb499bdadde3d83465907b34cf2c3
ansh batra <marsab@cpan.org>
2011-08-03 11:24:16 +0000

    Import of MARSAB/Bundle-FinalTest5 from CPAN.
    gitpan-cpan-distribution: Bundle
    gitpan-cpan-version:      FinalTest5
    gitpan-cpan-path:         MARSAB/Bundle-FinalTest5.tar.gz
    gitpan-cpan-author:       MARSAB
    gitpan-cpan-maturity:     released

--- lib/Bundle-FinalTest5.pm
+++ lib/Bundle-FinalTest5.pm
@@ -1,46 +1,31 @@
-package Bundle-FinalTest4;
+package Bundle-FinalTest5;
 
 use 5.010001;
 use strict;
 use warnings;
 
 require Exporter;
-
-our @ISA = qw(Exporter);
-our $VERSION = '0.01';
-
-
-# Preloaded methods go here.
-
 1;
 __END__
 # Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Bundle-FinalTest4 - Perl extension for blah blah blah
+Bundle-FinalTest5 - Perl extension for blah blah blah
 
 =head1 SYNOPSIS
 
-  use Bundle-FinalTest4;
-  blah blah blah
-
-=head1 DESCRIPTION
-
-Stub documentation for Bundle-FinalTest4, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
+  use Bundle-FinalTest5;
 =head1 CONTENTS
 AnyDBM_File
-Attribute::Handlers
-BerkeleyDB
-Class::Data::Inheritable
-Class::Singleton
+Attribute::Handlers 0.88
+BerkeleyDB 0.43
+Class::Data::Inheritable 0.08
+Class::Singleton 1.4
 Compress::Raw::Bzip2
 Compress::Raw::Zlib
 Crypt::OpenSSL::Random
-Crypt::RC4
+Crypt::RC4 2.02
 Data::Dump
 Date::Format
 Date::Parse
@@ -48,47 +33,47 @@ DB_File
 Digest::SHA
 Encode
 Exporter
-FCGI
-File::Grep
-File::Stat::Bits
+FCGI 0.71
+File::Grep 0.02
+File::Stat::Bits 1.01
 Filter::Util::Call
-GD
+GD 2.45
 GD::Text
 Geography::Countries
-Geo::IP
+Geo::IP 1.38
 Getopt::Std
-HTML::HTMLDoc
-HTML::Tagset
-Image::Magick
+HTML::HTMLDoc 0.10
+HTML::Tagset 3.20
+Image::Magick 6.5.1
 integer
 IO::Dir
 IO::File
 IO::Scalar
 IO::Socket::INET
-IO::Stringy
+IO::Stringy 2.110
 Locale::Maketext::Simple
 LWP::MediaTypes
 Math::Round
 MD5
-MIME::Base64
+MIME::Base64 3.13
 MIME::QuotedPrint
 MLDBM::Sync
-mod_perl2
+mod_perl2 2.0.4
 Net::CIDR::Lite
 Net::Domain
 Net::FTP
-Net::IP
+Net::IP 1.25
 Net::Ping
 Number::Compare
-OLE::Storage_Lite
+OLE::Storage_Lite 0.19
 Pod::Escapes
 Probe::Perl
-Scalar::Util::Numeric
-Storable
+Scalar::Util::Numeric 0.22
+Storable 2.25
 String::CRC32
 
 Sys::Hostname::Long
-Term::ReadKey
+Term::ReadKey 2.14
 Term::ReadLine
 Test
 Test::Harness
@@ -104,37 +89,37 @@ warnings
 Digest::Perl::MD5
 Encode::Locale
 ExtUtils::Install
-File::Path
-File::Slurp
+File::Path 
+File::Slurp 9999.13
 HTTP::Date
 IP::Country
 Test::Builder
 Test::More
-Test::Simple
+Test::Simple 0.98
 Text::Abbrev
 Crypt::OpenSSL::RSA
 Digest::base
 
 
 Algorithm::C3
-AppConfig
+AppConfig 1.66
 base
 Devel::Symdump
-Email::Date::Format
+Email::Date::Format 1.002
 File::Listing
 File::Type
 GD::Graph
 GD::SecurityImage
 IO::Pty
-JSON
+JSON 2.50
 JSON::PP
-List::MoreUtils
+List::MoreUtils 0.30
 List::Util
-MIME::Types
+MIME::Types 1.31
 MLDBM
 Module::CoreList
 Module::Load
-NetAddr::IP
+NetAddr::IP 4.044
 Package::Constants
 Params::Check
 parent
@@ -144,19 +129,19 @@ Pod::Usage
 Scalar::Util
 String::Approx
 Sub::Uplevel
-Test::Tester
+Test::Tester 0.107
 Text::Balanced
 Text::Glob
 Time::HiRes
-Try::Tiny
-URI
+Try::Tiny 0.09
+URI 1.56
 URI::Escape
 XSLoader
-Digest::SHA1
+Digest::SHA1 2.13
 Compress::Bzip2
 
 
-Class::Accessor
+Class::Accessor 0.34
 Compress::Zlib
 Cwd
 Error
@@ -164,26 +149,26 @@ File::Find::Rule
 File::Spec
 File::Spec::Functions
 Getopt::Long
-HTML::Parser
+HTML::Parser 3.68
 IO::Compress::Base
 IO::Compress::Bzip2
 IO::Compress::Gzip
 IO::Uncompress::Inflate
 IPC::Run
 IPC::Run3
-MIME::Lite
+MIME::Lite 3.027
 Pod::Coverage
 Test::Exception
-Test::Fatal
-Test::NoWarnings
+Test::Fatal 0.003
+Test::NoWarnings 1.02
 Test::Pod
 WWW::RobotRules
-Digest::MD5
-Email::Simple
+Digest::MD5 2.51
+Email::Simple 2.100
 
 
-CGI
-Config::General
+CGI 3.51
+Config::General 2.50
 CPAN::Meta::YAML
 Data::Compare
 Devel::StackTrace
@@ -195,27 +180,27 @@ Module::Pluggable
 Net::HTTP
 Pod::Man
 Test::Pod::Coverage
-Test::Deep
-Carp::Clan
+Test::Deep 0.108
+Carp::Clan 6.04
 Digest::StackTrace
-Digest::HMAC
+Digest::HMAC 1.02
 Digest::HMAC_MD5
 HTML::Template
 HTTP::Headers::Util
 HTTP::Status
 
 
-Archive::Tar
-Archive::Zip
+Archive::Tar 1.76
+Archive::Zip 1.30
 HTTP::Tiny
 Perl::OSType
-Template
+Template 2.22
 version
 Apache::Session
 CGI::Session
-Bit::Vector
+Bit::Vector 7.1
 Exception::Class
-ExtUtils::MakeMaker
+ExtUtils::MakeMaker 6.56
 HTTP::Cookies
 HTTP::Daemon
 HTTP::Headers
@@ -224,46 +209,46 @@ Test::Pod::Coverage
 
 Module::Load::Conditional
 Module::Metadata
-Net::DNS
+Net::DNS 0.66
 Parse::RecDescent
 Version::Requirements
 Class::C3
-Class::Load
-Date::Calc
-DBI
+Class::Load 0.06
+Date::Calc 6.3
+DBI 1.616
 HTTP::Negotiate
 MIME::Tools
 Parse::CPAN::Meta
 Test::Script
-XML::NamespaceSupport
-YAML
+XML::NamespaceSupport 1.11
+YAML 0.72
 YAML::Syck
 YAML::XS
 
 
 CPAN::Meta
 Date::Manip
-DBD::Pg
+DBD::Pg 2.17.2
 File::Which
 IPC::Cmd
-LWP
+LWP 5.837
 LWP::UserAgent
 Module::Signature
 MRO::Compat
-Spreadsheet::WriteExcel
-XML::SAX
+Spreadsheet::WriteExcel 2.37
+XML::SAX 0.96
 
 
 CPAN::Checksums
 Email::Abstract
-ExtUtils::CBuilder
+ExtUtils::CBuilder 0.2802
 File::HomeDir
-Spreadsheet::WriteExcel::Simple
+Spreadsheet::WriteExcel::Simple 1.04
 
 
 Email::Date
-ExtUtils::ParseXS
-Spreadsheet::ParseExcel
+ExtUtils::ParseXS 2.2206
+Spreadsheet::ParseExcel 0.58
 
 
 Module::Build
@@ -278,6 +263,14 @@ Params::Validate
 DateTime::Locale
 DateTime::TimeZone
 
+
+=head1 DESCRIPTION
+
+Stub documentation for Bundle-FinalTest5, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
 
 =head2 EXPORT
 
